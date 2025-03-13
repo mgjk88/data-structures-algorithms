a908b3ccbdffa0282fdf4e4bedef6bf7d79cac24
mgjk88 <159707804+mgjk88@users.noreply.github.com>
2025-03-14 00:20:09 +0800

    feat: add hoare partition in c++

---
+++ algorithms/sorting/quicksort/quicksort.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <functional>
+#include <iostream>
+
+int hoare(std::vector<int> *v, int leftIdx, int rightIdx){  
+    int pivotIdx = leftIdx;
+    int pivot = (*v)[leftIdx];
+    leftIdx++;
+    while(leftIdx <= rightIdx){
+        while((*v)[leftIdx] < pivot) leftIdx++;
+        while((*v)[rightIdx] > pivot) rightIdx--;
+        if(leftIdx < rightIdx){
+            std::swap((*v)[leftIdx], (*v)[rightIdx]);
+            leftIdx++;
+            rightIdx--;
+        }
+    }
+    std::swap((*v)[rightIdx], (*v)[pivotIdx]);
+    return rightIdx;
+}
+
+void quicksort(std::vector<int> *v, int leftIdx, int rightIdx, std::function<int(std::vector<int>*, int, int)> partition){
+    if(leftIdx >= rightIdx) return;
+    int partitionIdx = partition(v, leftIdx, rightIdx);
+    quicksort(v, leftIdx, partitionIdx - 1, partition);
+    quicksort(v, partitionIdx + 1,  rightIdx, partition);
+}
+
+int main() {
+    std::vector<int> v = {3, 2, 5, 1, 6, 2, 7, 8, 4, 9, 0};
+    for(auto i : v){
+        std::cout<<i<<' ';
+    }
+    std::cout<<'\n';
+    std::vector<int>* ptr = &v;
+    quicksort(ptr, 0, v.size() - 1, hoare);
+    for(auto i : v){
+        std::cout<<i<<' ';
+    }
+}
